8f57a60072dcceed8b00f0df043d87dbb675dd59
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2022-12-22 03:07:38 +0300

    DETERMINED contact model

---
+++ ClvrtcThirdTask/Models/ContactData.swift
@@ -0,0 +1,17 @@
+//
+//  ContactData.swift
+//  ClvrtcThirdTask
+//
+//  Created by Artyom Beldeiko on 21.12.22.
+//
+
+import Foundation
+import UIKit
+
+struct ContactData {
+    let givenName: String
+    let familyName: String
+    let phoneNumber: String
+    let image: UIImage
+    let isFavourite: Bool
+}
